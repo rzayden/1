5d9355b19fba9fde13c4e1ef2a8d9f2af4eb01a7
Zayden Kelsen <105763201+rzayden@users.noreply.github.com>
2022-05-23 00:56:46 +0000

    i added to the first question

---
+++ main.py
@@ -0,0 +1,6 @@
+print("welcome to my teo reo quiz!")
+# question 1 
+answer1 = input("How do you say hello in teo reo? \na. kia ora ]nb. tena koutou \nc pai \nAnswer: ") if answer1 == "a" or answer1 == "kia ora":
+print("correct")
+print("score: ", score)
+print("\n")
